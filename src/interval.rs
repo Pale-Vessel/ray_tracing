daa74afc32d60d6ac8d7805226153324a408e589
Vessel <waspsting@duck.com>
2025-08-25 10:46:40 +0100

    removed code annotated with #cfg(false)

--- src/interval.rs
+++ src/interval.rs
@@ -8,16 +8,9 @@ pub struct Interval {
 
 impl Interval {
     pub const EMPTY: Self = Self::new(f32::INFINITY, f32::NEG_INFINITY);
-    #[cfg(false)]
-    pub const UNIVERSE: Self = Self::new(f32::NEG_INFINITY, f32::INFINITY);
 
     pub fn size(&self) -> f32 {
         self.max - self.min
-    }
-
-    #[cfg(false)]
-    pub fn contains(&self, value: f32) -> bool {
-        self.min <= value && value <= self.max
     }
 
     pub fn surrounds(&self, value: f32) -> bool {
@@ -26,13 +19,6 @@ impl Interval {
 
     pub fn clamp(&self, value: f32) -> f32 {
         value.clamp(self.min, self.max)
-    }
-
-    #[cfg(false)]
-    pub fn expand(&self, delta: f32) -> Self {
-        #[cfg(false)]
-        let padding = delta / 2.;
-        Interval::new(self.min - padding, self.max + padding)
     }
 
     pub fn enclose(&self, interval_two: Self) -> Self {
