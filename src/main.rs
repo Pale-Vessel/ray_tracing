f63732705792b4690ebb63391f975927e4b36f9b
Vessel <waspsting@duck.com>
2025-08-29 16:19:28 +0100

    added cmdline arg for input scene path

--- src/main.rs
+++ src/main.rs
@@ -44,7 +44,7 @@ fn main() -> ImageResult<()> {
         Profile::ManyBounces => (800, 100, 50),
     };
     let (world, look_from, look_at, fov) =
-        scene_reader::reader::read_scene(String::from("spheres.scene"));
+        scene_reader::reader::read_scene(args[2].to_ascii_lowercase());
     let camera = Camera::initialise(
         image_width,
         rays_per_pixel,
