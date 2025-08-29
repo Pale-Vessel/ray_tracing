90965d91fb869463b436ff097dab542da6550ba8
Vessel <waspsting@duck.com>
2025-08-29 02:10:54 +0100

    added MulAssign to Colour

--- src/colour.rs
+++ src/colour.rs
@@ -3,17 +3,25 @@ use crate::{
     vector::VecRand,
 };
 
-use derive_more::{Add, Deref, Div, Mul, MulAssign, Sum};
+use std::ops::MulAssign;
+
+use derive_more::{Add, Deref, Div, Mul, Sum};
 use glam::{Vec2, Vec3};
 
 #[derive(
-    Clone, Copy, Debug, Default, Deref, Add, Sum, Mul, MulAssign, Div, PartialEq,
+    Clone, Copy, Debug, Default, Deref, Add, Sum, Mul, Div, PartialEq,
 )]
 pub struct Colour(Vec3);
 
 impl From<Vec2> for Colour {
     fn from(value: Vec2) -> Self {
         Self::new(value.x, 0., value.y)
+    }
+}
+
+impl MulAssign for Colour {
+    fn mul_assign(&mut self, rhs: Self) {
+        *self = Self(**self * *rhs)
     }
 }
 
