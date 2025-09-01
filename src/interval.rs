14ed1a5e4636a97535e2095cb6e44c7d8758a9d6
Vessel <waspsting@duck.com>
2025-09-01 15:14:37 +0100

    add new constant

--- src/interval.rs
+++ src/interval.rs
@@ -8,6 +8,7 @@ pub struct Interval {
 
 impl Interval {
     pub const EMPTY: Self = Self::new(f32::INFINITY, f32::NEG_INFINITY);
+    pub const UNIVERSE: Self = Self::new(f32::NEG_INFINITY, f32::INFINITY);
 
     pub fn size(&self) -> f32 {
         self.max - self.min
