0f48ab85199a8a7f69ee14dd7ac0df5da5cb2574
Vessel <waspsting@duck.com>
2025-09-01 12:25:40 +0100

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@ mod interval;
 mod scene_reader;
 mod textures;
 
-use crate::{camera::Camera, file_utils::clean_scenes::clean_scenes, scene_reader::reader::read_scene};
+use crate::{
+    camera::Camera, file_utils::clean_scenes::clean_scenes,
+    scene_reader::reader::read_scene,
+};
 use glam::Vec3;
 use image::ImageResult;
 
@@ -25,9 +28,8 @@ pub fn render(profile: &String, scene_name: &String) -> ImageResult<()> {
         }
         _ => panic!("Invalid profile"),
     };
-    let (world, look_from, look_at, fov) = read_scene(
-        format!("scenes/{}.scene", scene_name.to_ascii_lowercase()),
-    );
+    let (world, look_from, look_at, fov) =
+        read_scene(format!("scenes/{}.scene", scene_name.to_ascii_lowercase()));
     let camera = Camera::initialise(
         image_width,
         rays_per_pixel,
