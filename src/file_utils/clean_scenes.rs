93b06a54609da28eb4e5139284d14de61d9deee3
Vessel <waspsting@duck.com>
2025-09-05 11:06:19 +0100

    split up filecleaning to allow for adding more options

--- src/file_utils/clean_scenes.rs
+++ src/file_utils/clean_scenes.rs
@@ -4,10 +4,15 @@ pub fn clean_scenes() {
     for path in read_dir("scenes").unwrap() {
         let path = path.unwrap().path();
         let file = std::fs::read(&path).unwrap();
-        let lowered = file
-            .into_iter()
-            .map(|char| (char as char).to_ascii_lowercase())
-            .collect::<String>();
+        let lowered = clean_scene(file.into_iter().map(char::from).collect());
         std::fs::write(path, lowered).unwrap();
     }
+}
+
+fn clean_scene(scene: String) -> String {
+    lowercase(scene)
+}
+
+fn lowercase(scene: String) -> String {
+    scene.to_ascii_lowercase()
 }
